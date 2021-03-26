fbfabc88894b29b8fa84e141c7613780449bba22
Kirill <npabllla@gmail.com>
2021-03-26 18:25:38 +0300

    1. Написать тест на PostServlet. [#492569]

--- src/main/java/ru/job4j/dream/store/MemStore.java
+++ src/main/java/ru/job4j/dream/store/MemStore.java
@@ -2,16 +2,19 @@ package ru.job4j.dream.store;
 
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
+import ru.job4j.dream.model.User;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MemStore {
+public class MemStore implements Store {
     private static final AtomicInteger POST_ID = new AtomicInteger(4);
 
     private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
+    private static final AtomicInteger USER_ID = new AtomicInteger(4);
 
     private static final MemStore INST = new MemStore();
 
@@ -19,13 +22,9 @@ public class MemStore {
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
+    private final Map<Integer, User> users = new ConcurrentHashMap<>();
+
     private MemStore() {
-        posts.put(1, new Post(1, "Junior Java Job"));
-        posts.put(2, new Post(2, "Middle Java Job"));
-        posts.put(3, new Post(3, "Senior Java Job"));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static MemStore instOf() {
@@ -38,6 +37,11 @@ public class MemStore {
 
     public Collection<Candidate> findAllCandidates() {
         return candidates.values();
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
     }
 
     public void save(Post post) {
@@ -56,6 +60,14 @@ public class MemStore {
             candidate.setId(CANDIDATE_ID.incrementAndGet());
         }
         candidates.put(candidate.getId(), candidate);
+    }
+
+    @Override
+    public void save(User user) {
+        if (user.getId() == 0) {
+            user.setId(USER_ID.incrementAndGet());
+        }
+        users.put(user.getId(), user);
     }
 
     public Candidate findCandidateById(int id) {
