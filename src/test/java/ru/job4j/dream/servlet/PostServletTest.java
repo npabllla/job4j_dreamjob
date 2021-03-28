9e70666938c9ab124287c40ba24d7725c82a0371
Kirill <npabllla@gmail.com>
2021-03-28 18:08:46 +0300

    1. Написать тест на PostServlet. [#492569]

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -9,16 +9,20 @@ import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
 import ru.job4j.dream.store.Store;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(PsqlStore.class)
@@ -34,5 +38,20 @@ public class PostServletTest {
         when(req.getParameter("name")).thenReturn("Java job");
         new PostServlet().doPost(req, resp);
         assertThat(store.findAllPosts().iterator().next().getName(), is("Java job"));
+    }
+
+    @Test
+    public void doGetTest() throws ServletException, IOException {
+        Store store = MemStore.instOf();
+        PowerMockito.mockStatic(PsqlStore.class);
+        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        HttpSession session = mock(HttpSession.class);
+        when(req.getSession()).thenReturn(session);
+        when(req.getRequestDispatcher(any(String.class))).thenReturn(dispatcher);
+        new PostServlet().doGet(req, resp);
+        verify(req).getRequestDispatcher("/post/posts.jsp");
     }
 }
